5566824425006eb246f8474c3e3d8cb468221ffa
JaredLederman1 <jared.a.lederman@gmail.com>
2026-09-22 16:13:22 -0400

    Remove article block from home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
 import Hero from "@/components/Hero";
-import ArticleMagazine from "@/components/ArticleMagazine";
 import MissionSection from "@/components/MissionSection";
 import NewsletterSignup from "@/components/NewsletterSignup";
-import { getAllArticles } from "@/lib/articles";
 export default function Home() {
   return (
     <>
       <Hero />
-      <div className="container home-latest-articles">
-        <ArticleMagazine articles={getAllArticles()} showAllLink />
-      </div>
       <MissionSection />
       <NewsletterSignup />
     </>
